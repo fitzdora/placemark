5ca1d081e92d35e16461506539c5dd07b9f377f1
Isadora Fitzgerald <fitzdora@gmail.com>
2023-02-11 18:25:12 +0000

    Setting up Json

---
+++ src/models/json/sites-json-store.js
@@ -0,0 +1,46 @@
+import { v4 } from "uuid";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import { placeJsonStore } from "./place-json-store.js";
+
+const db = new Low(new JSONFile("./src/models/json/sites.json"));
+db.data = { sites: [] };
+
+export const siteJsonStore = {
+    async getAllSites() {
+        await db.read();
+        return db.data.sites;
+    },
+
+    async addSite(site) {
+        await db.read();
+        site._id = v4();
+        db.data.sites.push(site);
+        await db.write();
+        return site;
+    },
+     
+    async getSiteById(id) {
+        await db.read();
+        const list = db.data.sites.find((site) => site._id === id);
+        list.places = await placeJsonStore.getPlacesBySiteId(site._id);
+        return list;
+    },
+
+    async getUserSites(userid) {
+        await db.read();
+        return db.data.sites.filter((site) => site.userid === userid);
+    },
+
+    async deleteSiteById(id) {
+        await db.read();
+        const index = db.data.sites.findIndex((site) => site._id === id);
+        db.data.sites.splice(index, 1);
+        await db.write();
+    },
+
+    async deleteAllSites() {
+        db.data.sites = [];
+        await db.write();
+    },
+};
