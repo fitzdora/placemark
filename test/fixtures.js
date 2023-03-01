0b0bfeee8c01a9e2546dc3c4e38f046aed335771
Isadora Fitzgerald <fitzdora@gmail.com>
2023-03-01 13:34:40 +0000

    Set up create/delete user tests

---
+++ test/fixtures.js
@@ -0,0 +1,30 @@
+export const maggie = {
+    firstname: "Maggie",
+    lastname: "Simpson",
+    email: "maggie@simpson.com",
+    password:"secret",
+};
+
+export const testUsers = [
+{
+    firstname: "Homer",
+    lastname: "Simpson",
+    email: "homer@simpson.com",
+    password:"secret",
+
+},
+{
+    firstname: "Marge",
+    lastname: "Simpson",
+    email: "marge@simpson.com",
+    password:"secret",
+
+},
+{
+    firstname: "Bart",
+    lastname: "Simpson",
+    email: "bart@simpson.com",
+    password:"secret",
+
+},
+];
--- test/user-model-test.js
+++ test/user-model-test.js
@@ -1,21 +1,28 @@
 import { assert } from "chai";
 import { db } from "../src/models/db.js";
+import { maggie, testUsers } from "./fixtures.js";
 
 suite("User API tests", () => {
 
-    const maggie = {
-        firstname: "Maggie",
-        lastname: "Simpson",
-        email: "maggie@simpson.com",
-        password:"secret",
-    };
-
     setup(async () => {
         db.init();
+        await db.userStore.deleteAll();
     });
 
     test("create a user", async () => {
         const newUser = await db.userStore.addUser(maggie);
         assert.deepEqual(maggie, newUser)
+    });
+
+    test("delete all users", async () => {
+        for(let i = 0; i < testUsers.length; i += 1){
+            // eslint-disable-next-line no-await-in-loop
+            await db.userStore.addUser(testUsers[i]);
+        }
+        let returnedUsers = await db.userStore.getAllUsers();
+        assert.equal(returnedUsers.length, 3);
+        await db.userStore.deleteAll();
+        returnedUsers = await db.userStore.getAllUsers();
+        assert.equal(returnedUsers.length, 0);
     });
 });
