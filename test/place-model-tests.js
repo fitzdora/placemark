6c0c1f21d44a50e29451cd5c62d5ced1cdc18026
Isadora Fitzgerald <fitzdora@gmail.com>
2023-03-15 20:35:15 +0000

    implementing place test

--- test/place-model-tests.js
+++ test/place-model-tests.js
@@ -12,9 +12,9 @@ suite("Track Model tests", () => {
         await db.siteStore.deleteAllSites();
         await db.placeStore.deleteAllPlaces();
         gardenList = await db.siteStore.addSite(garden);
-        for (let i = 0; i < testPlaces.length; i += 1){
+        for (let i = 0; i < testPlaces.length; i += 1) {
             // eslint-disable-next-line no-await-in-loop
-            testPlaces[i] = await db.placeStore.addPlace(gardenList._id);
+            testPlaces[i] = await db.placeStore.addPlace(gardenList._id, testPlaces[i]);
         }
     });
 
@@ -30,4 +30,38 @@ suite("Track Model tests", () => {
         assert.equal(places.length, testPlaces.length)
     });
 
+    test("delete all places", async () => {
+        const places = await db.placeStore.getAllPlaces();
+        assert.equal(testPlaces.length, places.length);
+        await db.placeStore.deleteAllPlaces();
+        const newPlaces = await db.placeStore.getAllPlaces();
+        assert.equal(0, newPlaces.length);
+
+    });
+
+    test("get a place - success", async () => {
+        const fotaList = await db.siteStore.addSite(fota);
+        const place = await db.placeStore.addPlace(fotaList._id, castle)
+        const newPlace = await db.placeStore.getPlaceById(place._id);
+        assertSubset (castle, newPlace);
+    });
+
+    test("delete One Place - success", async () => {
+        await db.placeStore.deletePlace(testPlaces[0]._id);
+        const places = await db.placeStore.getAllPlaces();
+        assert.equal(places.length, testPlaces.length - 1);
+        const deletedPlace = await db.placeStore.getPlaceById(testPlaces[0]._id);
+        assert.isNull(deletedPlace);
+    });
+
+    test("get a place - bad params", async () => {
+        assert.isNull(await db.placeStore.getPlaceById(""));
+        assert.isNull(await db.placeStore.getPlaceById());
+    });
+
+    test("delete one place - fail", async () => {
+        await db.placeStore.deletePlace("bad-id");
+        const places = await db.placeStore.getAllPlaces();
+        assert.equal(places.length, testPlaces.length);
+    });
 });
